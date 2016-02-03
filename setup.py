32091ee5083da682a431ee1c0a05ed27160604ce
boumlaik <nboumlaik@advestis-conseil.com>
2016-02-03 17:24:14 +0100

    new

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ opts = {
 
                 'excludes': ['_gtkagg', '_tkagg', '_agg2', '_cairo', '_cocoaagg', "matplotlib.numerix.fft","sip", "PyQt4._qt",
                               "matplotlib.backends.backend_qt4agg",
-                               "matplotlib.numerix.linear_algebra", "matplotlib.numerix.random_array", '_fltkagg', '_gtk','_gtkcairo' ],
+                               "matplotlib.numerix.linear_algebra", "matplotlib.numerix.random_array", '_fltkagg', '_gtk','_gtkcairo' , 'IPython'],
               
               'dll_excludes': ['libgdk-win32-2.0-0.dll',
                                  'libgobject-2.0-0.dll',"MSVCP90.dll"]
@@ -36,4 +36,4 @@ opts = {
 ##                  (r'mpl-data\fonts',glob.glob(r'C:\Python26\Lib\site-packages\matplotlib\mpl-data\fonts\*.*'))]
 
 # for console program use 'console = [{"script" : "scriptname.py"}] 
-setup (windows = [{"script" : "ReadPlotCsv.py"}], options = opts,   data_files = matplotlib.get_py2exe_datafiles())
+setup (windows = [{"script" : "ReadPlotCsv_script.py"}], options = opts,   data_files = matplotlib.get_py2exe_datafiles())
